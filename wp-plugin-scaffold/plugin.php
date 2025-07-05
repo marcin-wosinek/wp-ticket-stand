4d85331e0c86a9dfae40eef1fd0b69acd783d46f
Marcin Wosinek <marcin.wosinek@gmail.com>
2025-07-05 12:51:20 +0200

    Initial commit

---
+++ wp-plugin-scaffold/plugin.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * WP Plugin Scaffold
+ *
+ * TODO: The description for the plugin scaffolded by this file.
+ *
+ * PHP version 8.2
+ *
+ * @category WordPress_Plugin
+ * @package  TODO
+ * @author   Marcin Wosinek <marcin.wosinek@gmail.com>
+ * @license  GPLv3 <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ * @link     https://github.com/marcin-wosinek/wp-plugin-scaffold
+ * @since    TODO: Date
+ *
+ * @wordpress-plugin
+ * Plugin Name: WP Plugin Scaffold
+ * Plugin URI:  https://github.com/marcin-wosinek/wp-plugin-scaffold
+ * Description: The description for the plugin scaffolded by this file.
+ * Author:      Marcin Wosinek <marcin.wosinek@gmail.com>
+ * Version:     1.0.0
+ */
+
+namespace WPPluginScaffold;
+
+defined('WPINC') || die;
+require_once __DIR__ . '/vendor/autoload.php';
